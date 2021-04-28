21737a770f2d19b8293abae6b7bb924b9f611c57
kaitbr0 <humansare@gmail.com>
2021-04-28 12:51:11 -0400

    deploy

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -8,7 +8,7 @@ import { ApolloProvider } from "@apollo/react-hooks";
 import { setContext } from "apollo-link-context";
 
 const httpLink = createHttpLink({
-  uri: "http://localhost:5000",
+  uri: "https://arcane-earth-91724.herokuapp.com/",
 });
 const authLink = setContext(() => {
   const token = localStorage.getItem("jwtToken");
